60eb37256593525dfc1e24143221a90c58142c39
nkey <lixiaodantogo@163.com>
2024-05-10 10:32:42 +0800

    first commit

---
+++ app/src/main/java/com/example/playaudiotest/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.example.playaudiotest
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.playaudiotest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+
+    private val mediaPlayer = MediaPlayer()
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        initMediaPlayer()
+        mBinding.play.setOnClickListener {
+            if (!mediaPlayer.isPlaying) {
+                mediaPlayer.start()
+            }
+        }
+        mBinding.pause.setOnClickListener {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.pause()
+            }
+        }
+        mBinding.stop.setOnClickListener {
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.reset()
+                initMediaPlayer()
+            }
+        }
+    }
+
+    private fun initMediaPlayer() {
+        val assetManager = assets
+        val fd = assetManager.openFd("music.mp3")
+        mediaPlayer.setDataSource(fd.fileDescriptor, fd.startOffset, fd.length)
+        mediaPlayer.prepare()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.stop()
+        mediaPlayer.release()
+    }
+
+}
